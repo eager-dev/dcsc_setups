63496f305220e4b408b50afbc77da8f3c09d86c1
Jelle <43064291+jelledouwe@users.noreply.github.com>
2022-02-08 17:24:23 +0100

    Update setup.py

--- eagerx_dcsc_setups/setup.py
+++ eagerx_dcsc_setups/setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='eagerx_dcsc_setups',
     version='0.0',
     description='Interface between EAGERx and the DCSC setups',
-    url='https://github.com/eager-dev/dcsc_setups/eagerx_d',
+    url='https://github.com/eager-dev/dcsc_setups/tree/main/eagerx_dcsc_setups',
     author='Bas van der Heijden, Jelle Luijkx',
     author_email='d.s.vanderheijden@tudelft.nl',
     license='Apache2.0',
